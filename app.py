a709596a501f0b15c5539bf42a37f5e4d5bbad23
robertoshimizu <rshimizubr@gmail.com>
2019-10-07 23:17:05 -0300

    fourth commit

--- app.py
+++ app.py
@@ -140,6 +140,7 @@ def login_required(f):
 
 # Logout
 @app.route('/logout')
+@login_required # going to check and use a function in decorator above
 def logout():
     session.clear()
     flash('You are now logged out', 'success')
@@ -150,6 +151,39 @@ def logout():
 @login_required # going to check and use a function in decorator above
 def dashboard():
     return render_template('dashboard.html')
+
+# Article Form Class from WTForms
+class ArticleForm(Form):
+    title = StringField('Title', [validators.Length(min=1, max=256)])
+    body = TextAreaField('Body', [validators.Length(min=30)])
+
+# Add Article
+@app.route('/add_article', methods=['GET', 'POST'])
+@login_required # going to check and use a function in decorator above
+def add_article():
+    form = ArticleForm(request.form)
+    if request.method == 'POST' and form.validate():
+        title = form.title.data
+        body = form.body.data
+
+        # Create Cursor
+        cur = mysql.connection.cursor()
+
+        # Execute
+        cur.execute("INSERT INTO articles(title, body, author) VALUES(%s, %s, %s)",(title, body, session['username']))
+
+        # Commit to DB
+        mysql.connection.commit()
+
+        #Close connection
+        cur.close()
+
+        flash('Article Created', 'success')
+
+        return redirect(url_for('dashboard'))
+
+    return render_template('add_article.html', form=form)
+   
 
 if __name__ == '__main__':
 
